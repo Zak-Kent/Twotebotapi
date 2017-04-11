e348f2342eff681a68a2f80fe6349f937805fb13
Zachary Kent <zak.kent@gmail.com>
2017-04-11 11:34:18 -0700

    moved Streambot class methods into utils funcs

--- twotebotapi/newbot.py
+++ twotebotapi/newbot.py
@@ -56,62 +56,10 @@ class StreamListener(tweepy.StreamListener):
             return False
 
 
-class BaseStreamBot:
-    """Base class with functionality not related to Tweepy and SUTime"""
-
-    def schedule_tweets(self, screen_name, tweet, tweet_id, talk_time):
-        """Schedule reminder tweets at set intervals"""
-
-        # check config table to see if autosend on
-        approved = db_utils.check_for_auto_send()
-
-        tweet_url = "https://twitter.com/{name}/status/{tweet_id}"
-        embeded_tweet = tweet_url.format(name=screen_name, tweet_id=tweet_id)
-
-        # set num of reminder tweets and interval in mins that tweets sent
-        # num_tweets = 2 & interval = 15 sends 2 tweets 30 & 15 mins before 
-        num_tweets = 2
-        interval = 1
-
-        for  mins in range(interval,(num_tweets*interval+1), interval):
-            remind_time = talk_time - timedelta(minutes=mins)
-            message = "Coming up in {} minutes! {}".format(mins, embeded_tweet)
-
-            tweet_obj = {
-                "message": message,
-                "approved": approved,
-                "remind_time": remind_time
-            }
-            print("message should be saved!!!")
-            print(tweet_obj)
-
-            # db_utils.save_outgoing_tweet(tweet_obj)
-
-    def convert_to_utc(self, talk_time):
-        """Convert the datetime string we get from SUTime to utcnow"""
-
-        # get correct local year, month, dat
-        local_date = datetime.now(self.tz)
-        local_date_str = datetime.strftime(local_date, "%Y %m %d")
-        year, month, day = local_date_str.split(" ")
-
-        # get SUTime parsed talk time and extract hours, mins
-        dt_obj = parse(talk_time)
-        local_time_str = datetime.strftime(dt_obj, "%H %M")
-        hours, mins = local_time_str.split(" ")
-        
-        # build up correct datetime obj, normalize & localize, switch to utc 
-        correct_dt = datetime(int(year), int(month), int(day), int(hours), int(mins))
-        tz_aware_local = self.tz.normalize(self.tz.localize(correct_dt))
-        local_as_utc = tz_aware_local.astimezone(pytz.utc)
-        
-        return local_as_utc
-
-
 class Streambot:
     """Stream Twitter and look for tweets that contain targeted words,
-    when tweets found look for datetime and room, if present save tweet to
-    OutgoingTweet model.
+    when tweets found look for datetime and room, if present save tweet 
+    to OutgoingTweet model.
 
     Ex.
     bot = Streambot()
@@ -130,21 +78,38 @@ class Streambot:
         """
         Set up auth stuff for api and return tweepy api object
         """
-        auth = tweepy.OAuthHandler(s.sender["CONSUMER_KEY"], s.sender["CONSUMER_SECRET"])
-        auth.set_access_token(s.sender["ACCESS_TOKEN"], s.sender["ACCESS_TOKEN_SECRET"])
-        api = tweepy.API(auth)
+        auth = tweepy.OAuthHandler(s.sender["CONSUMER_KEY"], 
+                                   s.sender["CONSUMER_SECRET"])
+        auth.set_access_token(s.sender["ACCESS_TOKEN"], 
+                              s.sender["ACCESS_TOKEN_SECRET"])
 
+        api = tweepy.API(auth)
         return api
 
-    def run_stream(self, search_list=[]):
+    def run_stream(self, search_list=None):
         """Start stream, when matching tweet found on_status method called. 
         search_list arg is a list of terms that will be looked for in tweets
         """
-        if search_list == []:
+        if search_list == None:
             raise ValueError("Need a list of search terms as arg to run_stream")
 
         stream = tweepy.Stream(auth=self.api.auth, listener=self.stream_listener)
         stream.filter(track=search_list)
+
+    def send_mention_tweet(self, screen_name):
+        """Mention a user in a tweet from bot letting them know that
+        their tweet has been recieved and that we will send out reminders
+        about thier event.
+        """
+        mention = "@{} We saw your openspaces tweet!".format(screen_name)
+        self.api.update_status(status=mention)
+
+    def parse_time_room(self, tweet):
+        """Get time and room number from a tweet using SUTime and tweet_utils
+        """
+        extracted_time = self.sutime.parse(tweet)
+        time_and_room = tweet_utils.get_time_and_room(tweet, extracted_time)
+        return time_and_room
 
     def retweet_logic(self, tweet, tweet_id, screen_name):
         """Use SUTime to try to parse a datetime out of a tweet, if successful
@@ -157,28 +122,13 @@ class Streambot:
         val_check = [val for val in time_room.values() if len(val) == 1]
 
         if len(val_check) == 2:
-            # way to mention a user after a valid tweet is recieved
-            # time_stamp = datetime.datetime.utcnow()
+            self.send_mention_tweet(screen_name)
 
-            # mention = "@{} We saw your openspaces tweet!{}".format(screen_name, time_stamp)
-
-            # self.api.update_status(status=mention)
-
-            # need to make time from SUTime match time Django is using
-            sutime_stuff = time_room["date"][0]
-            print("sutime_stuff: {}".format(sutime_stuff))
-            talk_time = time_utils.convert_to_utc(time_room["date"][0])
+            parsed_date = time_room["date"][0]
+            talk_time = time_utils.convert_to_utc(parsed_date)
             print("reult from convet to utc: {}".format(talk_time))
 
             test = tweet_utils.schedule_tweets(screen_name, tweet, tweet_id, talk_time)
-            
-    def parse_time_room(self, tweet):
-        """Get time and room number from a tweet using SUTime and tweet_utils
-        """
-        extracted_time = self.sutime.parse(tweet)
-        time_and_room = tweet_utils.get_time_and_room(tweet, extracted_time)
-        return time_and_room
-
 
 
 if __name__ == '__main__':
